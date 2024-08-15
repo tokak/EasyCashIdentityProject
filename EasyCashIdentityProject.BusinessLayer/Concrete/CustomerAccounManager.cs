56b0f257b80dc77b26856f7f736cdf89dbc25df5
Murat Tokak <tokakmurat01@gmail.com>
2024-08-16 02:38:06 +0300

    Business Katmanı ve Proje Tema Dosyaları

---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccounManager.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccounManager : ICustomerAccountService
+    {
+        private readonly ICustomerAccountDal _customerAccountDal;
+
+        public CustomerAccounManager(ICustomerAccountDal customerAccountDal)
+        {
+            _customerAccountDal = customerAccountDal;
+        }
+
+        public List<CustomerAccount> TGetList()
+        {
+            return _customerAccountDal.GetList();
+        }
+
+        public void TDelete(CustomerAccount t)
+        {
+            _customerAccountDal.Delete(t);
+        }
+
+        public CustomerAccount TGetByID(int id)
+        {
+            return _customerAccountDal.GetByID(id);
+        }
+
+        public void TInsert(CustomerAccount t)
+        {
+            _customerAccountDal.Insert(t);
+        }
+
+        public void TUpdate(CustomerAccount t)
+        {
+            _customerAccountDal.Update(t);
+        }
+    }
+}
---
+++ EasyCashIdentityProject.BusinessLayer/Concrete/CustomerAccountProcessManager.cs
@@ -0,0 +1,46 @@
+﻿using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.Concrete
+{
+    public class CustomerAccountProcessManager : ICustomerAccountProcessService
+    {
+        private readonly ICustomerAccounProcessDal _customerAccounProcessDal;
+
+        public CustomerAccountProcessManager(ICustomerAccounProcessDal customerAccounProcessDal)
+        {
+            _customerAccounProcessDal = customerAccounProcessDal;
+        }
+
+        public void TDelete(CustomerAccountProcess t)
+        {
+          _customerAccounProcessDal.Delete(t);
+        }
+
+        public CustomerAccountProcess TGetByID(int id)
+        {
+            return _customerAccounProcessDal.GetByID(id);
+        }
+
+        public List<CustomerAccountProcess> TGetList()
+        {
+            return _customerAccounProcessDal.GetList();
+        }
+
+        public void TInsert(CustomerAccountProcess t)
+        {
+            _customerAccounProcessDal.Insert(t);
+        }
+
+        public void TUpdate(CustomerAccountProcess t)
+        {
+            _customerAccounProcessDal.Update(t);
+        }
+    }
+}
